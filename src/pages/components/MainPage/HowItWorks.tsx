c885bb2386f1706dd72a91b68eefb0de6ee65ec2
Robert Moiseyev <moiseyevrobert@gmail.com>
2024-05-22 22:38:36 +0200

    made some changes

--- src/pages/components/MainPage/HowItWorks.tsx
+++ src/pages/components/MainPage/HowItWorks.tsx
@@ -9,23 +9,22 @@ export default function HowitWorks() {
         <>
 
 
-            <div id='how-it-works' className=" h-screen bg-secondBackgroundGray  flex  justify-center items-center ">
-                <div className=" bg-yellow-400 w-full  h-full flex justify-center text-white">
-                    <div className=" w-1/2 h-f  bg-black flex flex-col items-center justify-center text-4xl">
+            {/* <div id='how-it-works' className=" h-screen bg-secondBackgroundGray  flex  justify-center items-center ">
+                <div className=" w-full  h-full flex justify-center text-white">
+                    <div className=" w-1/2 h-f  flex flex-col items-center justify-center text-4xl">
 
                         <div><FcCalendar /></div>
                         <p className="  text-xl">Schedule a Consultation</p>
                         <div className="  text-2xl bg-textGray p-2 rounded-md">Step 1</div>
 
-                        {/* p */}
 
                     </div>
                 </div>
-                <div className=" bg-yellow-500 w-full  h-full flex justify-center text-white">
-                    <div className=" w-1/2  bg-black flex flex-col items-center justify-center text-4xl">
+                <div className="  w-full  h-full flex justify-center text-white">
+                    <div className=" w-1/2 flex flex-col items-center justify-center text-4xl">
 
 
-<div className=" flex flex-col items-center bg-blue-400 ">
+<div className=" flex flex-col items-center  ">
 
                         <div><FcSettings /></div>
                         <p className="  text-xl">Personalized Setup</p>
@@ -36,19 +35,18 @@ export default function HowitWorks() {
 
                     </div>
                 </div>
-                <div className=" bg-yellow-600 w-full h-full flex justify-center text-white">
-                    <div className=" w-1/2  bg-black flex flex-col items-center justify-center text-4xl">
+                <div className=" w-full h-full flex justify-center text-white">
+                    <div className=" w-1/2  flex flex-col items-center justify-center text-4xl">
 
                         <div><FcSportsMode /></div>
                         <p className="  text-xl">Prepared for Launch</p>
-                        <div className="  text-2xl bg-textGray p-2 rounded-md">Step 1</div>
+                        <div className="  text-2xl  bg-textGray p-2 rounded-md">Step 1</div>
 
-                        {/* p */}
-
+                     
                     </div>
                 </div>
 
-            </div>
+            </div> */}
 
         </>
     )
